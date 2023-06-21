581f89409b60468a24661b7d0ea31613d3d39050
Yumu0112 <yumu0112@icloud.com>
2023-06-21 11:20:49 +0900

    first step

---
+++ sql/users.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id int unsigned AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+INSERT INTO users (id, name) VALUES (1,'アーニャ');
+INSERT INTO users (id, name) VALUES (2,'錦木千束');
+INSERT INTO users (id, name) VALUES (3,'マーム');
